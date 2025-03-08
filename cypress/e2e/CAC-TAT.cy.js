50850292547ad7c962b852aaab2df24a6c5d2dff
leticiancalvess <leticiancalves1@gmail.com>
2025-03-07 21:33:48 -0300

    error message

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -15,5 +15,13 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#open-text-area').type(longText, { delay: 0 });
     cy.get('.button').click();
     cy.get('.success').should('be.visible');
+  }),
+  it('Exibe Mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type("Letícia");
+    cy.get('#lastName').type("Alves");
+    cy.get('#email').type("leticiancalves@x");
+    cy.get('#open-text-area').type("Hello");
+    cy.get('.button').click();
+    cy.get('.error').should('be.visible').should('contain', "Valide os campos obrigatórios!")
   })
 })
