d48b2499213167a69e1df92836fa2325abe33b07
leticiancalvess <leticiancalves1@gmail.com>
2025-03-08 15:11:28 -0300

    adding a file drag-drop

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -80,11 +80,19 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('input[type="checkbox"').check().should('be.checked').last().uncheck().should('not.be.checked')
     
   }),
-  it.only('seleciona um arquivo da pasta fixtures', () => {
+  it('seleciona um arquivo da pasta fixtures', () => {
     cy.get('#file-upload').selectFile('./cypress/fixtures/example.json')
     .should(input => {
       expect(input[0].files[0].name).to.equal('example.json')
     })
+  }),
+
+  it.only('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload').selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+    .should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
   })
+
 })
 
