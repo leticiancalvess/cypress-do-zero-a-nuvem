510c04ebedf430a3fc3d5dae50c8c74717b566fc
leticiancalvess <leticiancalves1@gmail.com>
2025-03-08 15:28:31 -0300

    invoke - opening another pages

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -93,12 +93,28 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       expect(input[0].files[0].name).to.equal('example.json')
     })
   }),
-  it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
     cy.fixture('example.json').as('myFile'); //nao precisa passar o caminho
     cy.get('#file-upload').selectFile('@myFile')
     .should(input => {
       expect(input[0].files[0].name).to.equal('example.json')
     })
+  }),
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .should('have.attr', 'href', 'privacy.html')
+    .and('have.attr', 'target', '_blank')
+  }),
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .invoke('removeAttr', 'target')
+    .click()
+
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  }),
+  it.only('testa a página da política de privacidade de forma independente', () => {
+    cy.visit('./src/privacy.html')
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
   })
 
 })
