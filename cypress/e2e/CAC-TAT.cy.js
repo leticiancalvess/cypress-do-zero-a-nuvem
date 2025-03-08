2ef7d277000b27150ac9699c5e23a217ac160c44
leticiancalvess <leticiancalves1@gmail.com>
2025-03-07 21:24:45 -0300

    type delay

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,19 @@
+beforeEach(() => {
+  cy.visit('../src/index.html');
+})
+
 describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
-    cy.visit('../src/index.html');
     cy.title().should('be.equal', "Central de Atendimento ao Cliente TAT")
 
+  }),
+  it('Preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('ajsdhajdsh', 20)
+    cy.get('#firstName').type("Letícia");
+    cy.get('#lastName').type("Alves");
+    cy.get('#email').type("leticiancalves@hotmail.com");
+    cy.get('#open-text-area').type(longText, { delay: 0 });
+    cy.get('.button').click();
+    cy.get('.success').should('be.visible');
   })
 })
