7b829deb7bcaeca83159a25bc7162feead658005
leticiancalvess <leticiancalves1@gmail.com>
2025-03-08 15:20:34 -0300

    adding a file fixture

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -87,8 +87,15 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     })
   }),
 
-  it.only('seleciona um arquivo simulando um drag-and-drop', () => {
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
     cy.get('#file-upload').selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+    .should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  }),
+  it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('myFile'); //nao precisa passar o caminho
+    cy.get('#file-upload').selectFile('@myFile')
     .should(input => {
       expect(input[0].files[0].name).to.equal('example.json')
     })
